5fecf51462d8e932936c78d1627d98fe7f561d61
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2026-08-17 16:54:20 +0300

    Add base main function

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,26 @@
 import 'reflect-metadata';
-
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
+import { promptPassword } from './utils/cli/prompt-password.util';
+import { CLI_COMMANDS } from './constants/cli-commands.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 
-void bootstrap();
+const authBootstrap = async () => {
+  promptPassword();
+};
+
+const main = async () => {
+  const command = process.argv[2];
+
+  if (command === CLI_COMMANDS.AUTH_BOOTSTRAP) {
+    await authBootstrap();
+  } else {
+    bootstrap();
+  }
+};
+
+main();
