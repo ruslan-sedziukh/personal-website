525de277523c1d30f7cab3db9be084c6f166be2c
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2026-08-27 22:26:24 +0300

    Add ts-ignore for import in test file

--- apps/api/src/utils/cli/prompt-password/test-prompt-password.ts
+++ apps/api/src/utils/cli/prompt-password/test-prompt-password.ts
@@ -1,4 +1,5 @@
 // TODO(auth): Figure out why extension here is required to test work
+// @ts-ignore: `.ts` extension is here intentionally. Test fails without it
 import { promptPassword } from './prompt-password.util.ts';
 
 await promptPassword();
